755b5ce04bd0c58dd382b9e58ea8fca5ddd1d4bc
Khanh <Khanh13Nguyen13@gmail.com>
2020-04-01 10:35:42 +0300

    Step by step

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  //= include/range-slider
+	//= include/markuplist
+  //= include/accordion
+	//= include/default
+	//= include/sliders
+	//= include/popup
+  //= include/forms
+	//= include/tabs
+	
+});
+
+
